fc24ce221e2d047f84e70f8c24586aec4ec949e9
stgo <stgo@pml.ac.uk>
2015-07-31 16:48:08 +0100

    Updated to output a config file of the form inputs, next the cron job

--- arsf_processing_page.py
+++ arsf_processing_page.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from numpy import arange
+import ConfigParser
+
+CONFIG_OUTPUT = "/data/turkana1/scratch/stgo/web_testing/configs/"
+UPLOAD_FOLDER = "/data/turkana1/scratch/stgo/web_testing/dem_upload/"
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "/users/rsg/stgo/upload_test/"
 
 bands = [1,2,3,4,5]
 PIXEL_SIZES = arange(0.5, 1.6, 0.1)
@@ -31,12 +36,65 @@ def test(name=None):
                            pixel_sizes=PIXEL_SIZES,
                            optimal_pixel=OPTIMAL_PIXEL,
                            bounds=bounds,
-                           name=name)
+                           name=name,
+                           julian_day=200,
+                           year=2014,
+                           project_code="T35T")
 
 @app.route('/jobrequestpost/', methods=['POST'])
 def post():
-    print request.form
-    print request.values
+    print "in"
+    requestdict = request.form
+    print "requested"
+    filename = "test.cfg"
+    config_output(requestdict, lines=[1, 2, 3, 4, 5], filename=filename)
+    print "working"
+    return "working"
+
+
+def config_output(requestdict, lines, filename):
+    print requestdict
+    print "configging"
+    config = ConfigParser.RawConfigParser()
+    config.add_section('global')
+    print "section"
+    config.set('global', "projection", requestdict["projectionRadios"])
+    config.set('global', "projstring", requestdict["projString"])
+    print "proj"
+    config.set('global', "dem", requestdict["optionsDemRadios"])
+    print "dem"
+    config.set('global', "bounds", requestdict["bound_n"] + ' ' +
+               requestdict["bound_e"] + ' ' +
+               requestdict["bound_s"] + ' ' +
+               requestdict["bound_w"])
+    print "dem and bounds"
+    config.set('global', "email", requestdict["email"])
+    print "email"
+    config.set('global', "interpolation", requestdict["optionsIntRadios"])
+    print "interp"
+    config.set('global', "pixelsize", requestdict["pixel_size_x"] + ' ' + requestdict["pixel_size_y"])
+    print "config set"
+    if requestdict["mask_all_check"] is "on":
+        masking = "all"
+    else:
+        masking = "some"
+    print "masked"
+    config.set('global', "masking", masking)
+    for line in lines:
+        print line
+        try:
+            config.add_section(str(line))
+        except Exception, e:
+            print e
+        print "section made"
+        if requestdict['%s_line_check' % line] is "on" or requestdict['process_all_lines'] is "on":
+            config.set(str(line), 'process', 'true')
+        else:
+            config.set(str(line), 'process', 'false')
+        config.set(str(line), 'band_range', requestdict["%s_band_start" % line] + ' - ' + requestdict["%s_band_stop" % line])
+    configfile = open(CONFIG_OUTPUT + filename, 'a')
+    config.write(configfile)
+    print config
     return "working"
 
 # def JobRequestPage(name=None):

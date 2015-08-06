4d5496096d67ed3236d2cb493be90c83f1e1f556
stgo <stgo@pml.ac.uk>
2015-08-06 17:18:55 +0100

    Updated to handle leading from another page, included config changes and better naming. Next is file upload.

--- arsf_processing_page.py
+++ arsf_processing_page.py
@@ -2,7 +2,14 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from numpy import arange
+import folder_structure
 import ConfigParser
+import glob
+import xml.etree.ElementTree as etree
+import os
+import utm
+import hdr_files
+import datetime
 
 CONFIG_OUTPUT = "/data/turkana1/scratch/stgo/web_testing/configs/"
 UPLOAD_FOLDER = "/data/turkana1/scratch/stgo/web_testing/dem_upload/"
@@ -25,68 +32,90 @@ bounds={
 def theme():
     return render_template('index.html', name=None)
 
+@app.route('/kmlpage')
+def kml_page(name=None):
+    return render_template('kml.html')
+
 @app.route('/')
-@app.route('/jobrequest')
-def test(name=None):
+@app.route('/jobrequest', methods=['GET', 'POST'])
+def jobr_request(name=None):
+    day=request.args["day"]
+    year=request.args["year"]
+    proj_code=request.args["project"]
+    try:
+        sortie = request.args["sortie"]
+    except:
+        sortie = ''
+    folder = folder_structure.FolderStructure(year=year, jday=str(day), projectCode=proj_code, fletter=sortie)
+    hyper_delivery = glob.glob(folder.projPath + '/delivery/*hyperspectral*')
+    projxml = etree.parse(glob.glob(hyper_delivery[0] + '/project_information/*project.xml')[0]).getroot()
+    bounds={
+        'n' : projxml.find('.//{http://www.isotc211.org/2005/gmd}northBoundLatitude').find('{http://www.isotc211.org/2005/gco}Decimal').text,
+        's' : projxml.find('.//{http://www.isotc211.org/2005/gmd}southBoundLatitude').find('{http://www.isotc211.org/2005/gco}Decimal').text,
+        'e' : projxml.find('.//{http://www.isotc211.org/2005/gmd}eastBoundLongitude').find('{http://www.isotc211.org/2005/gco}Decimal').text,
+        'w' : projxml.find('.//{http://www.isotc211.org/2005/gmd}westBoundLongitude').find('{http://www.isotc211.org/2005/gco}Decimal').text
+    }
+    utmzone = utm.from_latlon(float(bounds["n"]), float(bounds["e"]))[2:]
+    if utmzone[0] in [29, 30, 31] and utmzone[1] in ['U', 'V']:
+        britain = True
+    else:
+        britain=False
+    line_hdrs = [f for f in glob.glob(hyper_delivery[0] + '/flightlines/level1b/*.bil.hdr') if "mask" not in f]
+    lines = []
+    for line in line_hdrs:
+        linehdr = hdr_files.Header(line)
+        linedict = {
+            "name" : os.path.basename(line)[:-10],
+            "bandsmax" : int(linehdr.bands),
+            "bands" : range(1, int(linehdr.bands)+1, 1),
+        }
+        lines.append(linedict)
+    lines = sorted(lines, key=lambda line: line["name"])
     return render_template('requestform.html',
-                           flightlines=[1,2,3,4,5],
-                           uk = True,
-                           bands=bands,
-                           band_max=max(bands),
+                           flightlines=lines,
+                           uk = britain,
                            pixel_sizes=PIXEL_SIZES,
                            optimal_pixel=OPTIMAL_PIXEL,
                            bounds=bounds,
                            name=name,
-                           julian_day=200,
-                           year=2014,
-                           project_code="T35T")
+                           julian_day=day,
+                           year=year,
+                           project_code=proj_code,
+                           utmzone="UTM zone "+str(utmzone[0])+str(utmzone[1]))
 
-@app.route('/jobrequestpost/', methods=['POST'])
+@app.route('/progress', methods=['POST'])
 def post():
-    print "in"
     requestdict = request.form
-    print "requested"
     filename = "test.cfg"
-    config_output(requestdict, lines=[1, 2, 3, 4, 5], filename=filename)
-    print "working"
-    return "working"
+    lines = []
+    for key in requestdict:
+        if "_line_check" in key:
+            lines.append(key.strip("_line_check"))
+    lines = sorted(lines)
+    filename = requestdict["project_code"] + '_' + requestdict["year"] + '_' + requestdict["julianday"] + datetime.datetime.now()
+    config_output(requestdict, lines=lines, filename=filename)
+    return render_template('submitted.html')
 
 
 def config_output(requestdict, lines, filename):
-    print requestdict
-    print "configging"
     config = ConfigParser.RawConfigParser()
-    config.add_section('global')
-    print "section"
-    config.set('global', "projection", requestdict["projectionRadios"])
-    config.set('global', "projstring", requestdict["projString"])
-    print "proj"
-    config.set('global', "dem", requestdict["optionsDemRadios"])
-    print "dem"
-    config.set('global', "bounds", requestdict["bound_n"] + ' ' +
+    config.set('DEFAULT', "projection", requestdict["projectionRadios"])
+    config.set('DEFAULT', "projstring", requestdict["projString"])
+    config.set('DEFAULT', "dem", requestdict["optionsDemRadios"])
+    config.set('DEFAULT', "bounds", requestdict["bound_n"] + ' ' +
                requestdict["bound_e"] + ' ' +
                requestdict["bound_s"] + ' ' +
                requestdict["bound_w"])
-    print "dem and bounds"
-    config.set('global', "email", requestdict["email"])
-    print "email"
-    config.set('global', "interpolation", requestdict["optionsIntRadios"])
-    print "interp"
-    config.set('global', "pixelsize", requestdict["pixel_size_x"] + ' ' + requestdict["pixel_size_y"])
-    print "config set"
+    config.set('DEFAULT', "email", requestdict["email"])
+    config.set('DEFAULT', "interpolation", requestdict["optionsIntRadios"])
+    config.set('DEFAULT', "pixelsize", requestdict["pixel_size_x"] + ' ' + requestdict["pixel_size_y"])
     if requestdict["mask_all_check"] is "on":
         masking = "all"
     else:
         masking = "some"
-    print "masked"
-    config.set('global', "masking", masking)
+    config.set('DEFAULT', "masking", masking)
     for line in lines:
-        print line
-        try:
-            config.add_section(str(line))
-        except Exception, e:
-            print e
-        print "section made"
+        config.add_section(str(line))
         if requestdict['%s_line_check' % line] is "on" or requestdict['process_all_lines'] is "on":
             config.set(str(line), 'process', 'true')
         else:
@@ -94,8 +123,7 @@ def config_output(requestdict, lines, filename):
         config.set(str(line), 'band_range', requestdict["%s_band_start" % line] + ' - ' + requestdict["%s_band_stop" % line])
     configfile = open(CONFIG_OUTPUT + filename, 'a')
     config.write(configfile)
-    print config
-    return "working"
+    return 1
 
 # def JobRequestPage(name=None):
 #     utmzone = 12
@@ -110,4 +138,4 @@ def config_output(requestdict, lines, filename):
 #                            name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
